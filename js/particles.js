acbf129205d4522359b907b7b0c9049fd0bdb32b
Evan Hopper-Moore <evanhoppermoore@gmail.com>
2016-09-30 22:36:09 -0700

    Fix waves and add more of them

--- js/particles.js
+++ js/particles.js
@@ -1,6 +1,6 @@
 (function() {
-var d = new Date();
-var initTime;
+    var d = new Date();
+    var initTime;
     var Particle = function(x, y, r, vx, vy) {
         this.x = x || 0;
         this.y = y || 0;
@@ -8,30 +8,27 @@ var initTime;
         this.vx = vx || 0;
         this.vy = vy || 0;
         this.rand = Math.random();
-
-        this.update = function(vx, vy, spacing) {
+        console.log("particle init");
+        this.update = function(vx, vy, spacingx, spacingy) {
             vx = vx || 0,
                 vy = vy || 0;
             this.vx = this.vx;
             this.vy = this.vy;
-            this.y = (Math.cos(((new Date()).getTime() + this.x) / 180) * 50) + 50;
+            this.x = spacingx;
+            this.y = (Math.sin(((new Date()).getTime() + this.x) / 270) * 50) + spacingy;
         };
     };
 
-    var ParticleSystem = function(container, center, count, radius) {
+    var ParticleSystem = function(container, count, radius) {
         var ctx = container.getContext('2d');
 
         count = count || 0;
 
         this.particles = [];
 
-        this.center = {
-            x: center.x || 0,
-            y: center.y || 0
-        };
-
         // Initialization
         initTime = d.getTime();
+        console.log("init");
         for (i = 0; i < count; ++i) {
             var x = (i / count) * container.width,
                 y = (i / count) * container.height,
@@ -39,35 +36,41 @@ var initTime;
                 vy = 0
             this.particles.push(new Particle(x, y, radius, vx, vy));
         }
-
-        var particleChain (amount, canvas, length) {
-          var p = []
-          for(i = 0; i <= amount; i++){
-            p.push(new ParticleSystem(canvas, {
-                  x: canvas.width / 2,
-                  y: canvas.height / 2
-              }, 100, 6));
-          }
-        }
-
-        this.update = function() {
+        console.log("pushed particles");
+        this.update = function(updatedContainer, spacingy) {
             for (i = 0; i < count; ++i) {
                 if (this.particles[i].x < 0 &&
-                    this.particles[i].x > container.width &&
+                    this.particles[i].x > updatedContainer.width &&
                     this.particles[i].y < 0 &&
-                    this.particles[i].y > container.height) {
+                    this.particles[i].y > updatedContainer.height) {
 
-                    this.particles[i].x = (Math.random() * container.width);
+                    this.particles[i].x = (Math.random() * updatedContainer.width);
                     this.particles[i].y = 0;
                 }
-                this.particles[i].update();
-                ctx.fillStyle = "rgba(32, 45, 21, 0.3)";
+                var spacingx = (i / count) * updatedContainer.width;
+                this.particles[i].update(0, 0, spacingx, spacingy);
+                ctx.fillStyle = "rgba(32, 45, 21, 0.1)";
                 ctx.beginPath();
                 ctx.arc(this.particles[i].x, this.particles[i].y, this.particles[i].r, 0, Math.PI * 2, false);
                 ctx.fill();
             }
         };
     };
+
+    var particleChain = function(amount, canvas, length) {
+        this.p = [];
+        for (n = 0; n < amount; n += 1) {
+            var ps = new ParticleSystem(canvas, Math.round(Math.random() * 4 + length), 6);
+            this.p.push(ps);
+        }
+
+        this.update = function() {
+            for (a = 0; a < this.p.length; a++) {
+                var spacingy = (a / amount) * (canvas.width);
+                this.p[a].update(canvas, spacingy);
+            }
+        }
+    }
 
     window.requestAnimFrame = (function() {
         return window.requestAnimationFrame ||
@@ -92,7 +95,7 @@ var initTime;
         canvas.height = containerDiv.offsetHeight;
         canvas.width = containerDiv.clientWidth;
 
-        partChain = new particleChain(10, canvas, 25);
+        partChain = new particleChain(50, canvas, 15);
 
         paint();
 
@@ -100,7 +103,7 @@ var initTime;
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             canvas.height = containerDiv.offsetHeight;
             canvas.width = containerDiv.clientWidth;
-            p.update();
+            partChain.update();
 
             requestAnimFrame(paint);
         }
