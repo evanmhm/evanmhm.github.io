a956d25b576a12391e788deb43acc46074818900
Evan Hopper-Moore <evanhoppermoore@gmail.com>
2016-09-30 17:56:33 -0700

    Make particles a wave

--- js/particles.js
+++ js/particles.js
@@ -1,5 +1,6 @@
 (function() {
-
+var d = new Date();
+var initTime;
     var Particle = function(x, y, r, vx, vy) {
         this.x = x || 0;
         this.y = y || 0;
@@ -8,19 +9,17 @@
         this.vy = vy || 0;
         this.rand = Math.random();
 
-        this.update = function(vx, vy) {
+        this.update = function(vx, vy, spacing) {
             vx = vx || 0,
                 vy = vy || 0;
-            this.vx = this.rand;
-            this.vy = this.vx + this.rand;
-            this.x += this.vx + vx;
-            this.y += this.vy + vy;
+            this.vx = this.vx;
+            this.vy = this.vy;
+            this.y = (Math.cos(((new Date()).getTime() + this.x) / 180) * 50) + 50;
         };
     };
 
     var ParticleSystem = function(container, center, count, radius) {
-        var i = 0,
-            ctx = container.getContext('2d');
+        var ctx = container.getContext('2d');
 
         count = count || 0;
 
@@ -32,39 +31,40 @@
         };
 
         // Initialization
-        for (; i < count; ++i) {
-            var x = (Math.random() * container.width),
-                y = (Math.random() * container.height),
+        initTime = d.getTime();
+        for (i = 0; i < count; ++i) {
+            var x = (i / count) * container.width,
+                y = (i / count) * container.height,
                 vx = 0,
-                vy = 0;
-
+                vy = 0
             this.particles.push(new Particle(x, y, radius, vx, vy));
+        }
+
+        var particleChain (amount, canvas, length) {
+          var p = []
+          for(i = 0; i <= amount; i++){
+            p.push(new ParticleSystem(canvas, {
+                  x: canvas.width / 2,
+                  y: canvas.height / 2
+              }, 100, 6));
+          }
         }
 
         this.update = function() {
             for (i = 0; i < count; ++i) {
-                if (this.particles[i].x > 0 &&
-                    this.particles[i].x < container.width &&
-                    this.particles[i].y > 0 &&
-                    this.particles[i].y < container.height) {
+                if (this.particles[i].x < 0 &&
+                    this.particles[i].x > container.width &&
+                    this.particles[i].y < 0 &&
+                    this.particles[i].y > container.height) {
 
-                    this.particles[i].update();
-
-
-                    ctx.fillStyle = "rgba(32, 45, 21, 0.3)";
-                    ctx.beginPath();
-                    ctx.arc(this.particles[i].x, this.particles[i].y, this.particles[i].r, 0, Math.PI * 2, false);
-                    ctx.fill();
-                } else {
                     this.particles[i].x = (Math.random() * container.width);
                     this.particles[i].y = 0;
-                    this.particles[i].update();
-
-                    ctx.fillStyle = "rgba(32, 45, 21, 0.3)";
-                    ctx.beginPath();
-                    ctx.arc(this.particles[i].x, this.particles[i].y, this.particles[i].r, 0, Math.PI * 2, false);
-                    ctx.fill();
                 }
+                this.particles[i].update();
+                ctx.fillStyle = "rgba(32, 45, 21, 0.3)";
+                ctx.beginPath();
+                ctx.arc(this.particles[i].x, this.particles[i].y, this.particles[i].r, 0, Math.PI * 2, false);
+                ctx.fill();
             }
         };
     };
@@ -92,10 +92,7 @@
         canvas.height = containerDiv.offsetHeight;
         canvas.width = containerDiv.clientWidth;
 
-        p = new ParticleSystem(canvas, {
-            x: canvas.width / 2,
-            y: canvas.height / 2
-        }, 100, 6);
+        partChain = new particleChain(10, canvas, 25);
 
         paint();
 
